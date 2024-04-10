ba6bee65e9bebe32da60a927898f954abc2ddc3b
System Administrator <root@Songsuwins-MacBook-Pro.local>
2024-04-10 11:25:28 +0700

    Edit Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/aow-pruksa-ro/",
   plugins: [react()],
-  base: '/aow-pruksa-ro-landing-page/',
 });
